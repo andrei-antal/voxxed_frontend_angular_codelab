0fb83d530a50a698202f45f1ffe0d209a470bfad
Andrei Antal <antal.andrei@icloud.com>
2019-05-18 00:06:00 +0300

    Done step 2

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { MovieItemComponent } from './component/movie-item/movie-item.component';
 
 @NgModule({
   declarations: [MovieItemComponent],
   imports: [
+    FormsModule,
     CommonModule
   ],
   exports: [
